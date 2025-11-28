9405b7ab292824575274cabad7af30128eb36dca
Lemonboy060 <jalemon@hotmail.co.uk>
2025-11-28 14:20:58 +0000

    started create deck function in randomised algorithm file

--- randomised_algorithm.py
+++ randomised_algorithm.py
@@ -3,14 +3,22 @@ from tkinter import *
 class randomised():
     def __init__(self, parent_frame):
         self.parent_frame = parent_frame
-        deck_box = Text(self.parent_frame, width=60, height=3)
-        deck_box.place(x = 10, y = 35)
+        self.deck_box = Text(self.parent_frame, width=90, height=5)
+        self.deck_box.place(x = 10, y = 35)
 
-        Button(self.parent_frame, text ="Output Cards", font = (20)).place(x = 175, y = 150)
+        Button(self.parent_frame, text ="Output Cards", font = (20), command=self.create_deck).place(x = 175, y = 150)
         Button(self.parent_frame, text="Shuffle Cards", font = (20)).place(x = 295, y = 150)
     
-    def create_deck():
-        print()
+    def create_deck(self):
+        deck = []
+        suits = ["♥", "♦", "♣", "♠"]
+        cards = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"]
+
+        for suit in suits:
+            for card in cards:
+                deck.append(f"{card}, {suit}")
+        self.deck_box.insert(END, deck)
+        print() 
     
     def shuffle_deck():
         print()
