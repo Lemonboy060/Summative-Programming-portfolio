fe408655ee1f826187cbefc6d573d6077c93ec85
JLemon-06 <22jl2830@barton.ac.uk>
2025-11-30 21:04:36 +0000

    finished fibonacci sequence function

--- fibonacci_algorithm.py
+++ fibonacci_algorithm.py
@@ -8,4 +8,16 @@ class fibonacci():
         self.entry = Entry(parent_frame, width=35)
         self.entry.place(x = 142, y = 42)
 
-        Button(self.parent_frame, text="Find Nth Number", font = (20)).place(x = 175, y = 150)
+        Button(self.parent_frame, text="Find Nth Number", font = (20), command=self.fibonacci_creator).place(x = 175, y = 150)
+    
+    def fibonacci_creator(self):
+        nth_number = self.entry.get()
+        fibonacci_number_storage = [0, 1]
+        for length in range(int(nth_number)-2):
+            next = fibonacci_number_storage[length-1] + fibonacci_number_storage[length-2]
+            fibonacci_number_storage.append(next)
+        i = 0
+        for element in fibonacci_number_storage:
+            fibonacci_number_storage[i] = int(fibonacci_number_storage[i])
+            i += 1
+        Label(self.parent_frame, text = fibonacci_number_storage[nth_number], bg = "light blue", font = (20), fg = "black").place(x=290, y= 80)
