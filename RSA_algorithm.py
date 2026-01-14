6b13ba3748b57967181a5dd16f6f8fffd7c9573e
Jake Lemon <jalemon@hotmail.co.uk>
2026-01-14 17:47:14 +0000

    made notes for rsa, started rsa

--- RSA_algorithm.py
+++ RSA_algorithm.py
@@ -6,5 +6,65 @@ class RSA():
         Label(self.parent_frame, text = "Enter string to encrypt: ", bg = "light blue", font = (20), fg = "black").place(x=10, y=40)
         self.entry = Entry(parent_frame, width=35)
         self.entry.place(x = 202, y = 42)
+        Label(self.parent_frame, text = "Enter Public Key: ", bg = "light blue", font = (20), fg = "black").place(x=10, y=80)
+        self.entry_public_key = Entry(parent_frame, width=35)
+        self.entry_public_key.place(x = 202, y = 82)
+        Label(self.parent_frame, text = "Enter Private Key: ", bg = "light blue", font = (20), fg = "black").place(x=10, y=120)
+        self.entry_private_key = Entry(parent_frame, width=35)
+        self.entry_private_key.place(x = 202, y = 122)
 
-        Button(self.parent_frame, text="Count Pailndromes", font = (20)).place(x = 175, y = 165)
+        Label(self.parent_frame, text = "Outputted Text: ", bg = "light blue", font = (20), fg = "black").place(x=10, y=160)
+
+        Button(self.parent_frame, text="Encrypt", font = (20), command=self.rsa_menu).place(x = 175, y = 195)
+        Button(self.parent_frame, text="Decrypt", font = (20)).place(x = 260, y = 195)
+
+    def rsa_menu(self):
+        pub_key = self.entry_public_key.get()
+        pri_key = self.entry_private_key.get()
+
+        print(pub_key, pri_key)
+    
+    def generate_keys(self):
+        """
+        Used to generate both the public and private keys for enrypting and decrypting
+        user strings
+
+        To genertate keys
+            - Select two prime number, P1 and P2
+            - Calculate the product of thsoe primes N = p1 x p2
+            - Calculate the Totient of those primes T = (p1-1) x (p2-1)
+        
+        Then to select public key (B), select a random number
+            - Number must be prime
+            - be less then the totient B < T
+            - must not be a factor of the totient, T mod B != 0
+
+        Then to select private key (I), select random number
+            - Product of I (private) and B (public) divided by the totient must have a remainder of 1
+            so (B x I) mod T = 1    
+        :param self: Description
+        """
+    
+    def encrpyt(self):
+        """
+        Used public key to encrypt the message 
+        
+        to encrypt the message, the message value must be raised by the public key, then modded
+        by the product of the primes, to get the cipher text
+
+        Both encyption and decryption can also be done when swapping the key values
+
+        message^B mod N = cipher
+        """
+    
+    def decrypt(self):
+        """
+        uses private key to decrpyt the message
+
+        to decrypt the message, the cipher text value must be raised by the private key, then modded
+        by the product of the primes, to get the cipher text
+
+        Both encyption and decryption can also be done when swapping the key values
+        
+        Cipher^I mod N = message
+        """
--- main.py
+++ main.py
@@ -11,6 +11,15 @@ from RSA_algorithm import RSA
 
 class interface:
     def __init__(self, root):
+        """
+        intialising function used to create the tkinter window using root 
+
+        also intialises all the algorithm names, frame dictionary and all 
+        the frames to go in the dictionary as well as all the buttons to traverse
+
+        Args:
+        root (Tk): Main Tkinter function used to create all tkinter widgets and frames
+        """
         self.root = root
         self.all_algo = ["Search","Sorting","RSA","Factorial","Randomised","Brute force","Fibonacci","Palindrome"]
         self.frames_dict = {}
@@ -19,22 +28,48 @@ class interface:
         self.root.geometry("1200x800")
 
         self.create_intial_frame()
+        self.create_button_widgets()
         for algos in self.all_algo:
             self.create_all_frames(algos)
 
-        self.create_button_widgets()
         self.show_frame("Main menu")
 
     def show_frame(self, frame_name):
+        """
+        Core function which is used to show the current frame being accessed to the user
+        Accesses the actual frame stored in the fram dictionary so it can be used
+
+        Args:
+        frame_name (string):The name of the current frame/function which is being accessed
+        """
+
         frame = self.frames_dict[frame_name] 
         frame.tkraise()
 
     def create_intial_frame(self):
+        """
+        Creates the main menu frame, which all the buttons to navigate to other frames are
+        stored within 
+        
+        Then the main menu frame and its name are stored in the frames dictionary to be
+        accessed later
+        """
+
         self.main_menu_frame = Frame(self.root, bg = "light blue", width = 1200, height = 800)
         self.main_menu_frame.grid(row=0, column=0, sticky="nsew")
         self.frames_dict["Main menu"] = self.main_menu_frame
 
     def create_all_frames(self, algorithm_name):
+        """
+        Intialises all the frames and adds them to the frame dictionary with their names
+        so they may be accessed later 
+
+        Then depending on what algorithm/frame is being accessed then the function can
+        call that frames class
+        
+        Args:
+        algorithm_name (string): Specific name of the algorithm/frame being created
+        """
         self.frame = Frame(self.root, bg = "light blue", width = 1200, height = 800)
         self.frame.grid(row=0, column=0, sticky="nsew")
         self.frames_dict[algorithm_name] = self.frame
@@ -70,7 +105,7 @@ class interface:
         elif algorithm_name == "RSA":
             RSA(self.frame)
             Label(self.frame, text = f"{algorithm_name} Algorithm", bg = "light blue", font = (20), fg = "black").place(x = 10)
-            Button(self.frame, text="Back to Main Menu", font = (20), command=lambda: self.show_frame("Main menu")).place(x = 10, y = 165)
+            Button(self.frame, text="Back to Main Menu", font = (20), command=lambda: self.show_frame("Main menu")).place(x = 10, y = 195)
         else:
             Label(self.frame, text = f"{algorithm_name} Algorithm", bg = "light blue", font = (20), fg = "black").place(x = 10)
             Button(self.frame, text="Back to Main Menu", font = (20), command=lambda: self.show_frame("Main menu")).place(x = 10, y = 165)
@@ -80,12 +115,17 @@ class interface:
         """
         Creates all the button widgets for the mainmenu frame, allowing the user to traverese to
         different frames/classes
-        """
-        y_pos = 50
 
+        new_algo is created in the button creation line in order to correctly assign each button the
+        correct algorithm frame to show, otherwise all the buttons default to the palindrome frame
+        """
+
+        y_pos = 50
         Label(self.main_menu_frame, text = "Main menu - Select Algorithm", font = (20), bg = "light blue", fg = "black").place(x = 10)
+
         for algorithm in self.all_algo:
-            Button(self.main_menu_frame, text = f"{algorithm} Algorithm", font = (20), command=lambda: self.show_frame(algorithm)).place(x = 10, y = y_pos)
+            new_button = Button(self.main_menu_frame, text = f"{algorithm} Algorithm", font = (20), command=lambda new_algo=algorithm: self.show_frame(new_algo))
+            new_button.place(x = 10, y = y_pos)
             y_pos += 50
 
 if __name__ == "__main__":
